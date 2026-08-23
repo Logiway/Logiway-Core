84a8352599b295074634c74c48eb55518faf9b1f
EdwardAriaTanujaya <edwardariatanujaya@gmail.com>
2026-08-23 20:39:04 +0700

    feat: modify types

--- Logiway-BE/src/types/providers.ts
+++ Logiway-BE/src/types/providers.ts
@@ -1,17 +1,3 @@
-import type { GenerateContentParameters, GenerateContentResponse } from "@google/genai";
-
-export interface GeminiClient {
-  models: {
-    generateContent(params: GenerateContentParameters): Promise<GenerateContentResponse>;
-  };
-}
-
-export interface GeminiRepositoryOptions {
-  apiKey?: string;
-  client?: GeminiClient;
-  timeoutMs?: number;
-}
-
 export interface GraphHopperRepositoryOptions {
   url: string;
   fetchImplementation?: typeof fetch;
